4f9d0e05b7e372926719d82d6120489dfc6de676
Jorge <teactivo2025@gmail.com>
2025-10-24 13:15:18 -0300

    Modelo en BUILD

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@
 ollama serve &
 
 # 2. Espera unos segundos a que esté listo
-sleep 8
+sleep 5
 
 
 # 3. Descargando modelos si no existiesen
-echo "Asegurando que los modelos Ollama estén descargados..."
-ollama pull ${MODELO_EMBEDDINGS:-nomic-embed-text}
-ollama pull ${MODELO_LLM:-phi3:mini}
-echo "Modelos listos."
+# echo "Asegurando que los modelos Ollama estén descargados..."
+# ollama pull ${MODELO_EMBEDDINGS:-nomic-embed-text}
+# ollama pull ${MODELO_LLM:-phi3:mini}
+# echo "Modelos listos."
 
 
 # 3. Inicia la API de FastAPI en primer plano
